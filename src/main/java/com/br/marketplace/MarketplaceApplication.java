5d52d6209e13928dda9e2fbe770bcceb776285fe
Gabriel Teixeira Rocha Pires <pires.rocha011@gmail.com>
2024-09-13 07:56:21 -0300

    Adicionado Eureka

--- src/main/java/com/br/marketplace/MarketplaceApplication.java
+++ src/main/java/com/br/marketplace/MarketplaceApplication.java
@@ -2,8 +2,10 @@ package com.br.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
