54ac66f0f55c2ee045f52990bbece003cce0365d
Gabriel Teixeira Rocha Pires <pires.rocha011@gmail.com>
2024-09-13 09:14:32 -0300

    Retiração de anotação

--- src/main/java/com/br/marketplace/MarketplaceApplication.java
+++ src/main/java/com/br/marketplace/MarketplaceApplication.java
@@ -2,10 +2,8 @@ package com.br.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
